a97c0bc9dd07be8871045e78b530e4379364d97a
ysonu1071 <ysonu1071@gmail.com>
2023-10-14 16:03:38 +0530

    start script added

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // mongoose.connect("mongodb://0.0.0.0:27017/todos")
-console.log(process.env["DB_URI"]);
+
 mongoose.connect(process.env["DB_URI"])
 .then(()=> console.log("DB connected"))
 .catch((error)=>{
