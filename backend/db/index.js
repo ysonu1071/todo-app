cf3306e5bea208b9f120f28bd463b2a166acb8af
ysonu1071 <ysonu1071@gmail.com>
2023-10-14 15:38:16 +0530

    mongodb altlas url added

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://0.0.0.0:27017/todos")
+// mongoose.connect("mongodb://0.0.0.0:27017/todos")
+mongoose.connect("mongodb+srv://quora-clone:root@cluster0.itbsa47.mongodb.net/todos")
 .then(()=> console.log("DB connected"))
 .catch((error)=>{
     console.log(error.message)
