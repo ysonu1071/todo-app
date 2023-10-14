6950be7880382c9e9c03984e8b63199f1e923b8f
ysonu1071 <ysonu1071@gmail.com>
2023-10-14 15:51:23 +0530

    dotenv added

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 // mongoose.connect("mongodb://0.0.0.0:27017/todos")
-mongoose.connect("mongodb+srv://quora-clone:root@cluster0.itbsa47.mongodb.net/todos")
+console.log(process.env["DB_URI"]);
+mongoose.connect(process.env["DB_URI"])
 .then(()=> console.log("DB connected"))
 .catch((error)=>{
     console.log(error.message)
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("./db/index");
